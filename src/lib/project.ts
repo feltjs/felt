750fbf02888cfd437521ba9967f8b5618153c35d
Ryan Atkinson <mail@ryanatkn.com>
2023-01-27 10:33:11 -0700

    move one

--- src/lib/project.ts
+++ src/lib/project.ts
@@ -4,12 +4,12 @@ export const project = {
 		'sustainability through aligned incentives',
 		'intentional collaboration, community, and governance',
 		'open source',
-		'going with the grain of the web',
 		'transparency and accountability',
 		'simplicity and efficiency',
 		'easy and inexpensive self-hosting',
 		'privacy by default',
 		'accessibility and inclusivity',
+		'going with the grain of the web',
 		'fostering learning, innovation, and experimentation',
 	],
 };
