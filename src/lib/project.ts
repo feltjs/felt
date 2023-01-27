60fc94a566d6bb99a62fb110043478b794ebe9da
Ryan Atkinson <mail@ryanatkn.com>
2023-01-26 18:32:18 -0700

    add project values

---
+++ src/lib/project.ts
@@ -0,0 +1,15 @@
+export const project = {
+	values: [
+		'users come first',
+		'sustainability through aligned incentives',
+		'positive-sum collaboration, community, and governance',
+		'open source',
+		'transparency and accountability',
+		'simplicity and efficiency',
+		'easy self-hosting',
+		'privacy by default',
+		'accessibility and inclusivity',
+		'growth mindset with continuous improvement and learning',
+		'fostering innovation and experimentation',
+	],
+};
