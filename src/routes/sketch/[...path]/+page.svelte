d9323920c4b4fec7396e1115c9ce14310a02393b
Ryan Atkinson <mail@ryanatkn.com>
2023-02-01 00:22:34 -0700

    redirect

---
+++ src/routes/sketch/[...path]/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import {browser} from '$app/environment';
+	import {page} from '$app/stores';
+	import {goto} from '$app/navigation';
+
+	// redirect old sketch links to ui.felt.dev
+	if (browser) void goto('https://ui.felt.dev' + $page.url.pathname);
+</script>
+
+ok
