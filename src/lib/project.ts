7a102e1a582153ec90cd894e1e8b293ed4bd7a59
Ryan Atkinson <mail@ryanatkn.com>
2023-01-31 23:49:10 -0700

    wip

--- src/lib/project.ts
+++ src/lib/project.ts
@@ -1,4 +1,55 @@
-export const project = {
+export interface Project {
+	repos: Repo[];
+	values: string[];
+	operators: Operator[];
+}
+
+export interface Repo {
+	name: string;
+	summary: string;
+	url: string;
+	source: string;
+}
+
+export interface Operator {
+	name: string;
+	url: string;
+}
+
+export const project: Project = {
+	repos: [
+		{
+			name: 'Felt',
+			summary: 'the website for the Felt project (you are here)',
+			url: 'https://www.felt.dev/',
+			source: 'https://github.com/feltjs/felt',
+		},
+		{
+			name: '@feltjs/felt-server',
+			summary: 'server for Felt, a tool for building and maintaining communities',
+			url: 'https://server.felt.dev/',
+			source: 'https://github.com/feltjs/felt-server',
+		},
+		{
+			name: '@feltjs/felt-ui',
+			summary: 'styles and Svelte UI components',
+			url: 'https://ui.felt.dev/',
+			source: 'https://github.com/feltjs/felt-ui',
+		},
+		{
+			name: 'felt-template',
+			summary:
+				'a static web app and Node library template with Svelte, SvelteKit, Vite, TypesScript, ESLint, Prettier, Gro, and Felt',
+			url: 'https://template.felt.dev/',
+			source: 'https://github.com/feltjs/felt-template',
+		},
+		{
+			name: '@feltjs/felt-util',
+			summary: 'a collection of JS utilities to complement the modern web platform',
+			url: 'https://util.felt.dev/',
+			source: 'https://github.com/feltjs/felt-util',
+		},
+	],
 	values: [
 		'we put the people who use our software first',
 		'sustainability through aligned incentives',
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,36 @@
 <script lang="ts">
+	import {stripEnd} from '@feltjs/util/string.js';
+	import {base} from '$app/paths';
+
+	import {project} from '$lib/project';
 </script>
 
 <main class="markup column">
 	<section>
 		<header class="centered-hz">
+			<img src="{base}/favicon.png" alt="green felt textured heart with the word 'felt' cut out" />
 			<h1><a href="https://github.com/feltjs">felt</a></h1>
 		</header>
+	</section>
+	<section>
+		<table>
+			<tbody
+				>{#each project.repos as repo}<tr
+						><td>{repo.name}</td><td>{repo.summary}</td><td
+							><a href={repo.url}>{stripEnd(repo.url.substring(8), '/')}</a></td
+						><td><a href={repo.source}>src</a></td></tr
+					>{/each}</tbody
+			>
+		</table>
+	</section>
+	<section class="centered">
+		<h3 style="width: 100%">values:</h3>
+		<ul>
+			{#each project.values as value}<li>{value}</li>{/each}
+		</ul>
+	</section>
+	<section class="centered">
+		<p><a href="https://github.com/feltjs/felt">source code</a></p>
 	</section>
 </main>
 
@@ -18,5 +43,23 @@
 	}
 	h1 {
 		text-align: center;
+	}
+	table {
+		font-size: var(--font_size_lg);
+	}
+	tr {
+		background-color: var(--tint_dark_0);
+	}
+	tr:nth-child(2n) {
+		background-color: var(--tint_dark_1);
+	}
+	tr:hover {
+		background-color: var(--tint_light_0);
+	}
+	td {
+		padding: var(--spacing_xs) var(--spacing_md);
+	}
+	td:first-child {
+		white-space: nowrap;
 	}
 </style>
