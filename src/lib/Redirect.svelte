3aae8206ab64974d8c335500c688a825b0bdaf8a
Ryan Atkinson <mail@ryanatkn.com>
2023-02-01 00:37:49 -0700

    redirect

---
+++ src/lib/Redirect.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import {browser} from '$app/environment';
+	import {goto} from '$app/navigation';
+	import {page} from '$app/stores';
+
+	// TODO is there a better way to do this with static websites? searching Kit docs came up short
+
+	export let host = 'https://ui.felt.dev';
+
+	$: url = host + $page.url.pathname;
+
+	// redirect old sketch links to ui.felt.dev
+	if (browser) void goto(url);
+</script>
+
+redirect to <a href={url}>{url}</a>
