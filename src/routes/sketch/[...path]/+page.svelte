bb9eec8e9a0c93a15b5bfc40b75d25f5f3e42e06
Ryan Atkinson <mail@ryanatkn.com>
2023-02-01 00:34:09 -0700

    redirect

--- src/routes/sketch/[...path]/+page.svelte
+++ src/routes/sketch/[...path]/+page.svelte
@@ -3,6 +3,8 @@
 	import {page} from '$app/stores';
 	import {goto} from '$app/navigation';
 
+	// TODO is there a better way to do this with static websites? searching Kit docs came up short
+
 	const redirect = 'https://ui.felt.dev' + $page.url.pathname;
 
 	// redirect old sketch links to ui.felt.dev
