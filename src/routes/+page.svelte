11c955b2d69f56eb09620e889bcaf06f35dd4d90
Ryan Atkinson <mail@ryanatkn.com>
2023-02-01 15:51:40 -0700

    add governance doc link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -35,6 +35,9 @@
 		<ul>
 			{#each project.values as value}<li>{value}</li>{/each}
 		</ul>
+		<p style="width: 100%">
+			See <a href="https://github.com/feltjs/felt/blob/main/GOVERNANCE.md">GOVERNANCE.md</a> for more.
+		</p>
 	</section>
 	<footer class="centered">
 		<p><a href={project.source}>source code</a></p>
