c0d1670b0ef3575af24eda8703f6070cfef7294a
Ryan Atkinson <mail@ryanatkn.com>
2023-02-14 09:54:53 -0700

    upgrade

--- src/routes/sketch/library/+page.svelte
+++ src/routes/sketch/library/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import Redirect from '$routes/Redirect.svelte';
+	import Redirect from '@feltjs/felt-ui/Redirect.svelte';
 </script>
 
 <Redirect host="https://ui.felt.dev" path="/library" />
