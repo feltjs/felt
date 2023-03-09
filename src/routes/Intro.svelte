72405b6eab8eb51d9db235a91121f03c56e41311
Ryan Atkinson <mail@ryanatkn.com>
2023-03-08 22:39:53 -0700

    restructure intro

--- src/routes/Intro.svelte
+++ src/routes/Intro.svelte
@@ -1,7 +1,5 @@
 <p class="panel padded-xl">
 	<strong>Felt</strong>
-	is a
-	<small><code>work in progress</code></small> toolkit of free and open source software. In our mission
-	to make our lives better with technology, we design and code tools that make it simple and easy to
-	build and maintain communities, publish to the web, and collaborate with information 💚
+	is a free and open source toolkit for building and maintaining communities, publishing to the web,
+	and collaborating with information. Our mission to make our lives better with technology 💚
 </p>
