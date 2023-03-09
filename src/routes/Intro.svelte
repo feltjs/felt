1c4bb6f2092fadc3fa2d4f55939f2c8fd9a1e8bc
Ryan Atkinson <mail@ryanatkn.com>
2023-03-08 22:57:13 -0700

    tweak

--- src/routes/Intro.svelte
+++ src/routes/Intro.svelte
@@ -1,5 +1,5 @@
 <p class="panel padded-xl">
 	<strong>Felt</strong>
 	is a free and open source toolkit for building and maintaining communities, publishing to the web,
-	and collaborating with information. Our mission to make our lives better with technology 💚
+	and collaborating with information. Felt's mission to make our lives better with technology 💚
 </p>
