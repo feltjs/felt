28e5c9dea3c9f2d27345ddbf4917bdee5164e0eb
Ryan Atkinson <mail@ryanatkn.com>
2023-03-08 22:39:07 -0700

    add operator to the footer

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -3,6 +3,13 @@
 </script>
 
 <footer>
+	<div class="panel markup">
+		<div>operated by Felt Co-op</div>
+		<div>
+			<a href="https://www.felt.social/">felt.social</a>&nbsp;
+			<a href="https://github.com/feltcoop">@feltcoop</a>
+		</div>
+	</div>
 	<slot />
 	<SocialLinks />
 </footer>
@@ -14,5 +21,12 @@
 		align-items: center;
 		flex-direction: column;
 		margin-top: 35px;
+	}
+
+	.panel {
+		padding: var(--spacing_lg);
+		display: flex;
+		flex-direction: column;
+		align-items: center;
 	}
 </style>
